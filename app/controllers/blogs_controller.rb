9537ba8e8ef6753ad181f82b9882e8c0605beb6f
Ryan Andersen <ryan.andersen117@gmail.com>
2017-06-20 15:32:33 +0000

    added functionality for publish/draft

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status
+  ]
 
   # GET /blogs
   # GET /blogs.json
@@ -57,7 +58,20 @@ class BlogsController < ApplicationController
     end
   end
 
+def toggle_status
+  
+if @blog.draft? 
+  @blog.published! 
+else
+  @blog.draft!
+end
+
+  redirect_to blogs_path, notice: "Post status updated"
+end
+
+
   private
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
       @blog = Blog.friendly.find(params[:id])
