3126bebc90f3431432a82bdd51264b863f291c5c
Ryan Andersen <ryan.andersen117@gmail.com>
2017-06-28 17:25:51 +0000

    Added nav bar, along with defualt header content

---
+++ app/controllers/concerns/default_page_content.rb
@@ -0,0 +1,11 @@
+module DefaultPageContent
+  extend ActiveSupport::Concern
+  
+  included do
+  before_filter :set_page_defaults
+end
+  def set_page_defaults
+    @page_title = "Ryan Andersen | Portfolio"
+    @seo_keywords = "Ryan Andersen portfolio"
+  end
+end
